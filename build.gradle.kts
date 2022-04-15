6681b679a430cd3dc798351149a6f441ab23ea69
Joo <joo@joostory.net>
2022-04-15 11:33:02 +0900

    kotlin querydsl 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
   kotlin("jvm") version "1.6.10"
   kotlin("plugin.jpa") version "1.6.10"
   kotlin("plugin.allopen") version "1.6.10"
+  kotlin("kapt") version "1.6.20"
   java
+  idea
 }
 
 group = "net.joostory"
@@ -17,9 +19,19 @@ dependencies {
 
   implementation("com.h2database:h2:2.1.210")
   implementation("org.hibernate:hibernate-entitymanager:5.6.7.Final")
+  implementation("com.querydsl:querydsl-jpa:5.0.0")
+  kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
 
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+idea {
+  module {
+    val kaptMain = file("build/generated/source/kapt/main")
+    sourceDirs.add(kaptMain)
+    generatedSourceDirs.add(kaptMain)
+  }
 }
 
 tasks.getByName<Test>("test") {
